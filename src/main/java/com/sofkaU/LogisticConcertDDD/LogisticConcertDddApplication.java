8879e64becf1de17a5ba00865dfd36c29f57c47c
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-18 09:14:51 -0500

    Initializing project
    First commit

---
+++ src/main/java/com/sofkaU/LogisticConcertDDD/LogisticConcertDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.LogisticConcertDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticConcertDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticConcertDddApplication.class, args);
+	}
+
+}
